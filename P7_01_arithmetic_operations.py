d292380b5fd1d9cdaff9d5511b77fbd93e0d7102
gandli <gandli@qq.com>
2023-12-20 00:04:00 +0800

    ✨ feat:新功能 | New feature

--- P7_01_arithmetic_operations.py
+++ P7_01_arithmetic_operations.py
@@ -3,26 +3,26 @@
 """
 
 # 定义两个数字
-A = 10
-B = 3
+NUMBER1 = 13
+NUMBER2 = 2
 
 # 加法
-print(f"加法结果:{A+B}")
+print(f"加法结果:{NUMBER1+NUMBER2}")
 
 # 减法
-print(f"减法结果:{A-B}")
+print(f"减法结果:{NUMBER1-NUMBER2}")
 
 # 乘法
-print(f"乘法结果:{A*B}")
+print(f"乘法结果:{NUMBER1*NUMBER2}")
 
 # 除法（浮点数结果）
-print(f"除法结果:{ A / B }")
+print(f"除法结果:{ NUMBER1 / NUMBER2 }")
 
 # 整数除法
-print(f"整数除法结果:{ A // B }")
+print(f"整数除法结果:{ NUMBER1 // NUMBER2 }")
 
 # 取余
-print(f"取余结果:{ A % B }")
+print(f"取余结果:{ NUMBER1 % NUMBER2 }")
 
 # 幂运算
-print(f"幂运算结果:{ A ** B }")
+print(f"幂运算结果:{ NUMBER1 ** NUMBER2 }")
